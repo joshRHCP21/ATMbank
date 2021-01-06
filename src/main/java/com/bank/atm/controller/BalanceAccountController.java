32cbf26f262d8d49f110610b300f06bbb0d68d3b
Jose Antonio Alvino Velasque <jalvinov@everis.com>
2021-01-06 12:26:21 -0500

    Swagger implemented

--- src/main/java/com/bank/atm/controller/BalanceAccountController.java
+++ src/main/java/com/bank/atm/controller/BalanceAccountController.java
@@ -4,6 +4,10 @@ import com.bank.atm.entity.BalanceAccount;
 import com.bank.atm.entity.DocumentRequest;
 import com.bank.atm.service.BalanceAccountService;
 import io.reactivex.Single;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +15,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/atm")
-public class BalanceAccountController {
+@Api("/atm")
+public class BalanceAccountController
+{
     @Autowired
     private final BalanceAccountService balanceAccountService;
 
     @PostMapping("/deposits")
+    @ApiOperation(value = "Check Balance Account by Document Number", notes = "Retrieving the Balance Account ", response = BalanceAccount.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = BalanceAccount.class)
+    })
     public Single<BalanceAccount> consultBalanceAcountByDocumentNumber(@RequestBody DocumentRequest documentRequest)
     {
         return balanceAccountService.consultBalanceAccount(documentRequest.getDocument());
